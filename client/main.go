c8c0128d2c450b3716514b721ed210f90177cfed
rozy <rozy@arbeon.com>
2023-09-11 18:46:43 +0900

    add: base server, client code

--- client/main.go
+++ client/main.go
@@ -2,134 +2,48 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
 	"fmt"
-	"log"
+	"net"
 	"os"
 	"strings"
-
-	"github.com/gorilla/websocket"
 )
-
-var (
-	serverAddr = "ws://localhost:8080/ws" // 채팅 서버 주소 (서버 주소에 맞게 수정하세요)
-)
-
-type Message struct {
-	Sender  string `json:"sender"`
-	Content string `json:"content"`
-}
 
 func main() {
-	conn, _, err := websocket.DefaultDialer.Dial(serverAddr, nil)
+	fmt.Print("Enter server address (e.g., localhost:8080): ")
+	serverAddress := readInput()
+
+	conn, err := net.Dial("tcp", serverAddress)
 	if err != nil {
-		log.Fatalf("Could not connect to server: %v", err)
+		fmt.Println("Error connecting:", err.Error())
+		os.Exit(1)
 	}
 	defer conn.Close()
 
-	fmt.Println("Connected to the chat server")
-
-	go readMessages(conn)
-
-	for {
-		messageText := getUserInput()
-		if messageText == "/list" {
-			// "/list" 명령어를 입력하면 서버에 채널 목록 요청을 보냅니다.
-			listChannels(conn)
-			fmt.Printf("리스트 조회")
-		} else if strings.HasPrefix(messageText, "/create ") {
-			// "/create" 명령어를 사용하여 새 채널을 생성합니다.
-			channelName := strings.TrimSpace(strings.TrimPrefix(messageText, "/create "))
-			createChannel(conn, channelName)
-			fmt.Printf("채널 생성")
-		} else if strings.HasPrefix(messageText, "/join ") {
-			// "/join" 명령어를 사용하여 이미 존재하는 채널에 참여합니다.
-			channelName := strings.TrimSpace(strings.TrimPrefix(messageText, "/join "))
-			joinChannel(conn, channelName)
-		} else {
-			// 메시지를 입력하면 메시지를 서버에 전송합니다.
-			sendMessage(conn, messageText)
+	go func() {
+		for {
+			message, err := bufio.NewReader(conn).ReadString('\n')
+			if err != nil {
+				fmt.Println("Server disconnected.")
+				os.Exit(0)
+			}
+			fmt.Print(message)
 		}
-	}
-}
+	}()
 
-func getUserInput() string {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter message: ")
-	message, _ := reader.ReadString('\n')
-	return strings.TrimSpace(message)
-}
-
-func readMessages(conn *websocket.Conn) {
 	for {
-		_, msg, err := conn.ReadMessage()
+		fmt.Print("Enter command: ")
+		command := readInput()
+
+		_, err := conn.Write([]byte(command + "\n"))
 		if err != nil {
-			log.Fatalf("Error reading message: %v", err)
-			return
-		}
-		fmt.Printf("Received: %s\n", msg)
-	}
-}
-
-func createChannel(conn *websocket.Conn, channelName string) {
-	// 서버에게 새 채널 생성 요청을 보냅니다.
-	request := Message{
-		Sender:  "YourName",
-		Content: "/create " + channelName,
-	}
-	sendJSONMessage(conn, request)
-	// 서버로부터 응답을 기다립니다.
-	response := Message{}
-	err := conn.ReadJSON(&response)
-	if err != nil {
-		log.Fatalf("Error reading server response: %v", err)
-	}
-
-	// 서버로부터 받은 응답에 따라 처리합니다.
-	if response.Sender == "Server" {
-		if strings.Contains(response.Content, "created successfully") {
-			fmt.Println("Channel created successfully.")
-		} else if strings.Contains(response.Content, "same name already exists") {
-			fmt.Println("Channel with the same name already exists.")
-		} else {
-			fmt.Println("Server response: " + response.Content)
+			fmt.Println("Error sending data:", err.Error())
+			os.Exit(1)
 		}
 	}
 }
 
-func listChannels(conn *websocket.Conn) {
-	// 서버에게 채널 목록 요청을 보냅니다.
-	request := Message{
-		Sender:  "YourName",
-		Content: "/list",
-	}
-	sendJSONMessage(conn, request)
-}
-
-func joinChannel(conn *websocket.Conn, channelName string) {
-	// 서버에게 채널 목록 요청을 보냅니다.
-	request := Message{
-		Sender:  "YourName",
-		Content: "/join " + channelName,
-	}
-	sendJSONMessage(conn, request)
-}
-
-func sendMessage(conn *websocket.Conn, messageText string) {
-	message := Message{
-		Sender:  "YourName",
-		Content: messageText,
-	}
-	sendJSONMessage(conn, message)
-}
-
-func sendJSONMessage(conn *websocket.Conn, message Message) {
-	messageJSON, err := json.Marshal(message)
-	if err != nil {
-		log.Fatalf("Error encoding message as JSON: %v", err)
-	}
-	err = conn.WriteMessage(websocket.TextMessage, messageJSON)
-	if err != nil {
-		log.Fatalf("Error sending message: %v", err)
-	}
+func readInput() string {
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	return strings.TrimSpace(input)
 }
--- server/main.go
+++ server/main.go
@@ -1,212 +1,131 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
-	"net/http"
+	"net"
+	"os"
 	"strings"
 	"sync"
-
-	"github.com/gorilla/websocket"
 )
 
-type Message struct {
-	Sender  string `json:"sender"`
-	Content string `json:"content"`
+type Group struct {
+	Name     string
+	Members  map[net.Conn]struct{}
+	Messages chan string
 }
 
-var (
-	upgrader = websocket.Upgrader{
-		CheckOrigin: func(r *http.Request) bool {
-			return true
-		},
-	}
+var groups = make(map[string]*Group)
+var mu sync.Mutex
 
-	clients  = make(map[*websocket.Conn]string)
-	channels = make(map[string][]*websocket.Conn)
-	mutex    = sync.Mutex{}
-)
+func handleClient(conn net.Conn) {
+	defer conn.Close()
 
-func handleConnections(w http.ResponseWriter, r *http.Request) {
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer ws.Close()
+	clientAddr := conn.RemoteAddr().String()
+	fmt.Printf("Client %s connected.\n", clientAddr)
 
-	username := r.URL.Query().Get("username")
-	group := r.URL.Query().Get("group")
+	conn.Write([]byte("Welcome to the chat server!\n"))
+	conn.Write([]byte("Enter '/join <group_name>' to join a group.\n"))
 
-	mutex.Lock()
-	clients[ws] = username
-	channels[group] = append(channels[group], ws)
-	mutex.Unlock()
-
-	fmt.Printf("%s joined group %s\n", username, group)
+	var currentGroup *Group
 
 	for {
-		var msg Message
-		err := ws.ReadJSON(&msg)
+		buffer := make([]byte, 1024)
+		n, err := conn.Read(buffer)
 		if err != nil {
-			log.Printf("error: %v", err)
-			delete(clients, ws)
-			removeFromGroup(group, ws)
-			break
+			fmt.Printf("Client %s disconnected.\n", clientAddr)
+			if currentGroup != nil {
+				leaveGroup(currentGroup, conn)
+			}
+			return
 		}
-		msg.Sender = username
-		if strings.HasPrefix(msg.Content, "/create ") {
-			// "/create" 명령어를 처리합니다.
-			channelName := strings.TrimSpace(strings.TrimPrefix(msg.Content, "/create "))
-			handleCreateChannel(ws, username, group, channelName)
-			fmt.Printf("채널 생성")
-		} else if strings.HasPrefix(msg.Content, "/list") {
-			handleListChannels(ws)
-		} else if strings.HasPrefix(msg.Content, "/join ") {
-			// "/join" 명령어를 사용하여 이미 존재하는 채널에 참여합니다.
-			channelName := strings.TrimSpace(strings.TrimPrefix(msg.Content, "/join "))
-			joinChannel(ws, username, group, channelName)
-			fmt.Printf("채널 입장")
+
+		message := string(buffer[:n])
+		message = strings.TrimSpace(message)
+
+		if strings.HasPrefix(message, "/join ") {
+			groupName := strings.TrimPrefix(message, "/join ")
+			groupName = strings.TrimSpace(groupName)
+
+			mu.Lock()
+			if group, ok := groups[groupName]; ok {
+				group.Members[conn] = struct{}{}
+				currentGroup = group
+				mu.Unlock()
+				conn.Write([]byte(fmt.Sprintf("Joined group '%s'\n", groupName)))
+				go func() {
+					group.Messages <- fmt.Sprintf("Client %s joined the group.\n", clientAddr)
+				}()
+			} else {
+				mu.Unlock()
+				conn.Write([]byte(fmt.Sprintf("Group '%s' does not exist. Create it using '/create <group_name>'.\n", groupName)))
+			}
+		} else if strings.HasPrefix(message, "/create ") {
+			groupName := strings.TrimPrefix(message, "/create ")
+			groupName = strings.TrimSpace(groupName)
+
+			mu.Lock()
+			if _, ok := groups[groupName]; !ok {
+				newGroup := &Group{
+					Name:     groupName,
+					Members:  make(map[net.Conn]struct{}),
+					Messages: make(chan string, 100),
+				}
+				newGroup.Members[conn] = struct{}{}
+				groups[groupName] = newGroup
+				currentGroup = newGroup
+				mu.Unlock()
+				conn.Write([]byte(fmt.Sprintf("Created and joined group '%s'\n", groupName)))
+				go func() {
+					newGroup.Messages <- fmt.Sprintf("Client %s created and joined the group.\n", clientAddr)
+				}()
+				go broadcastGroupMessages(newGroup)
+			} else {
+				mu.Unlock()
+				conn.Write([]byte(fmt.Sprintf("Group '%s' already exists. Join it using '/join <group_name>'.\n", groupName)))
+			}
+		} else if currentGroup != nil {
+			go func() {
+				currentGroup.Messages <- fmt.Sprintf("Client %s: %s", clientAddr, message)
+			}()
 		} else {
-			// 일반 메시지를 브로드캐스트합니다.
-			msg.Sender = username
-
-			broadcast(group, msg)
+			conn.Write([]byte("Invalid command. Enter '/join <group_name>' to join a group or '/create <group_name>' to create a new group.\n"))
 		}
 	}
 }
 
-func removeFromGroup(group string, conn *websocket.Conn) {
-	mutex.Lock()
-	defer mutex.Unlock()
-	for i, c := range channels[group] {
-		if c == conn {
-			channels[group] = append(channels[group][:i], channels[group][i+1:]...)
-			break
+func leaveGroup(group *Group, conn net.Conn) {
+	mu.Lock()
+	delete(group.Members, conn)
+	if len(group.Members) == 0 {
+		close(group.Messages)
+		delete(groups, group.Name)
+	}
+	mu.Unlock()
+}
+
+func broadcastGroupMessages(group *Group) {
+	for message := range group.Messages {
+		for conn := range group.Members {
+			conn.Write([]byte(message + "\n"))
 		}
-	}
-}
-
-func broadcast(group string, msg Message) {
-	mutex.Lock()
-	channelClients := channels[group]
-	mutex.Unlock()
-	for _, client := range channelClients {
-		err := client.WriteJSON(msg)
-		if err != nil {
-			log.Printf("error: %v", err)
-		}
-	}
-}
-
-func getChannelList() []string {
-	mutex.Lock()
-	defer mutex.Unlock()
-
-	var channelList []string
-	for channelName := range channels {
-		channelList = append(channelList, channelName)
-	}
-	return channelList
-}
-
-func handleListChannels(conn *websocket.Conn) {
-	// 서버에서 현재 존재하는 채널 목록을 가져옵니다.
-	channelList := getChannelList()
-
-	// 채널 목록을 클라이언트에게 전송합니다.
-	response := Message{
-		Sender:  "Server",
-		Content: "Channel List: " + strings.Join(channelList, ", "),
-	}
-	sendJSONMessage(conn, response)
-}
-func handleCreateChannel(conn *websocket.Conn, username string, group string, channelName string) {
-	// Mutex를 사용하여 동시성 문제를 방지합니다.
-	mutex.Lock()
-	defer mutex.Unlock()
-
-	// 이미 같은 이름의 채널이 존재하는지 확인합니다.
-	if _, exists := channels[channelName]; exists {
-		response := Message{
-			Sender:  "Server",
-			Content: "Channel with the same name already exists.",
-		}
-		sendJSONMessage(conn, response)
-		return
-	}
-
-	// 새 채널을 생성합니다.
-	channels[channelName] = []*websocket.Conn{conn}
-
-	// 클라이언트에게 성공 메시지를 전송합니다.
-	response := Message{
-		Sender:  "Server",
-		Content: fmt.Sprintf("Channel '%s' created successfully.", channelName),
-	}
-	sendJSONMessage(conn, response)
-
-	// 사용자를 새 채널에 추가합니다.
-	clients[conn] = username
-	conn.WriteJSON(Message{
-		Sender:  "Server",
-		Content: fmt.Sprintf("You joined the channel '%s'.", channelName),
-	})
-
-	// 사용자가 채널에 참여했음을 다른 사용자에게 알립니다.
-	broadcast(channelName, Message{
-		Sender:  "Server",
-		Content: fmt.Sprintf("%s joined the channel.", username),
-	})
-}
-
-func joinChannel(conn *websocket.Conn, username string, group string, channelName string) {
-	mutex.Lock()
-	defer mutex.Unlock()
-
-	if _, exists := channels[channelName]; !exists {
-		response := Message{
-			Sender:  "Server",
-			Content: fmt.Sprintf("Channel '%s' does not exist.", channelName),
-		}
-		sendJSONMessage(conn, response)
-		return
-	}
-
-	channels[channelName] = append(channels[channelName], conn)
-	clients[conn] = username
-	conn.WriteJSON(Message{
-		Sender:  "Server",
-		Content: fmt.Sprintf("You joined the channel '%s'.", channelName),
-	})
-
-	broadcast(channelName, Message{
-		Sender:  "Server",
-		Content: fmt.Sprintf("%s joined the channel.", username),
-	})
-}
-
-func sendJSONMessage(conn *websocket.Conn, message Message) {
-	messageJSON, err := json.Marshal(message)
-	if err != nil {
-		log.Fatalf("Error encoding message as JSON: %v", err)
-	}
-	err = conn.WriteMessage(websocket.TextMessage, messageJSON)
-	if err != nil {
-		log.Fatalf("Error sending message: %v", err)
 	}
 }
 
 func main() {
-	http.HandleFunc("/ws", handleConnections)
+	listen, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	defer listen.Close()
+	fmt.Println("Chat server is listening on localhost:8080")
 
-	go func() {
-		err := http.ListenAndServe(":8080", nil)
+	for {
+		conn, err := listen.Accept()
 		if err != nil {
-			log.Fatal("ListenAndServe: ", err)
+			fmt.Println("Error accepting connection:", err.Error())
+			os.Exit(1)
 		}
-	}()
-
-	fmt.Println("Group Chat Server started on :8080")
-	select {}
+		go handleClient(conn)
+	}
 }
